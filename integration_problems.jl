1369da20522dfcbe92a7831f567514e8571b73dc
Yash-10 <yashgondhalekar567@gmail.com>
2021-08-03 19:07:29 +0530

    Add question 2 attempt

--- integration_problems.jl
+++ integration_problems.jl
@@ -20,3 +20,38 @@ function ar1TimeSeries(T, alphas)
 end
 ts = ar1TimeSeries(T, alphas)
 display(plot!(1:T, ts, label=["0" "0.3" "0.9"]))  # Can make label generalized?
+
+
+## Under construction ##
+# 2. Logistic Map
+# b(n+1) = r * bn * (1 - bn)
+function logistic()
+    r = 2.9
+    r_end = 4
+    dr = 0.001
+    rs = []
+    while r != r_end
+        r = r + dr
+        push!(rs, r)
+
+    yrs = []
+    for r in range_
+        bn = 0.25
+        l = []
+        yr = []
+        push!(l, bn)
+        for i in 1:400+150
+            bn = r * bn * (1 - bn)
+            if i >= 400
+                push!(yr, bn)
+            else
+                push!(l, bn)
+            end
+        end
+        push!(yrs, yr)
+    end
+    return rs, yrs
+end
+
+yrs = logistic()
+display(plot!(rs, yrs))
