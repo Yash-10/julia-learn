af842f95ecaa9ebae352d11610b2da3b6a8d0b8a
Yash-10 <yashgondhalekar567@gmail.com>
2021-08-06 00:58:34 +0530

    Update intermediate solutions

--- intermediate_problems.jl
+++ intermediate_problems.jl
@@ -1,6 +1,9 @@
+using Unitful
+
 # MyRange Part 1:
 
 # Part (i)
+# Types cannot be redefined. So `MyRange` defined below cannot be redefined anywhere in this module.
 struct MyRange
     start::Int
     step::Int
@@ -33,3 +36,88 @@ end
 # Checking
 println(arr[3]) # 5
 println(arr[10]) # 19
+
+
+# Part 2
+# Lazy implementation of LinSpace.
+
+# Ellipsis for varargs - variable number of arguments.
+function Base.getindex(a::MyRange, i...)
+    answer = Int[]
+    for index in i
+        push!(answer, a[index])
+    end
+    return answer
+end
+
+arr = MyRange(1, 2, 20)
+println(arr[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# Checking with built-in `range`.
+for value in range(1, stop=19, length=10)
+    println(value)
+end
+
+
+# Part 3
+struct UnitStepRange
+    start::Int
+    step::Int
+    stop::Int
+end
+
+
+
+"""
+x - The input value at which the interpolated value needs to be found.
+start, step, stop - Parameters for the custom range type.
+====
+Handles the case where no interpolation is needed.
+"""
+function interpolate(x::Float64, start::Int, step::Int, stop::Int)
+    arr = start:step:stop
+
+    for ival in enumerate(arr)
+        if ival[1] == x
+            y = arr[ival[1]]
+            return y
+        elseif ival[1] > x
+            y1 = arr[ival[1]-1] # first
+            y2 = arr[ival[1]] # last
+            x1 = floor(x)
+            x2 = ceil(x)
+            y = y1 + ((y2 - y1) / (x2 - x1)) * (x - x1)
+            return y
+        end
+    end
+end
+
+
+# Linear Interpolation: y = y1 + ((y2 - y1) / (x2 - x1)) * (x - x1)
+
+# Call overload.
+(c::UnitStepRange)(x::Float64) = interpolate(x, c.start, c.step, c.stop)
+usr = UnitStepRange(1, 2, 10)
+
+println("Interpolated value at 1.5 is $(usr(1.5))")
+println("Interpolated value at 1.4 is $(usr(1.4))")
+println("Interpolated value at 2.3 is $(usr(2.3))")
+# No interpolation case.
+println("Interpolated value at 2.0 is $(usr(2.0))")
+
+
+# Part 4
+arr = [103.456, 100, 1000.103, 3402.568, 9027.34921, 9012834.23] * Unitful.N
+# Using above LinSpace on "unitful" arrays gives an error. So we can overload `Base.getindex` to allow unitful arrays.
+"""
+This function doesn't work yet
+"""
+function Base.getindex(a::MyRange, i...) where {T<:Unitful.N}
+    answer = []
+    for index in i
+        push!(answer, a[index])
+    end
+    return answer
+end
+
+println(arr[1, 3])
