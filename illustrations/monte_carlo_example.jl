b12efff9d261d6e02a8f46f9d6ecebb5de1642ee
Yash-10 <yashgondhalekar567@gmail.com>
2021-09-22 15:31:37 +0530

    Add illustrations

---
+++ illustrations/monte_carlo_example.jl
@@ -0,0 +1,32 @@
+# Suppose we want to calculate the integral: \theta = \(\int_{0}^{1} sqrt(1 - y^2) \,dy\).
+# For demonstration and proof of concept, we could convert this integral = E[1 - U^2], where U is a uniformly distributed random variable.
+# Uniform random variables can be easily simulated by a computer.
+
+using Distributions;
+
+sample_means = []
+
+"""
+n: No. of times the experiment must be repeated.
+"""
+function approx_integral(n)
+    sampleMeans = []
+    for i in 1:n
+        for i in 1:100
+            x = []
+            append!(x, sqrt(1 - rand(Uniform(0, 1)) ^ 2))
+        append!(sampleMeans, mean(x))
+        end
+    return mean(sampleMeans)
+    end
+end
+
+# Do the experiment n >= 30 times.
+for n in [30, 100, 10000, 1000000]
+    approxValue = approx_integral(200)
+    println("n = ", n)
+    println("================")
+    println("Expected: ", pi/4)
+    println("Approximation: ", approxValue)
+    print("\n")
+end
